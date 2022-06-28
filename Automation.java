c8f09be4f0849af7e8e98c72cfcb7c4772d01b99
Sakthipradeep <105548072+sakthipradeep@users.noreply.github.com>
2022-06-27 21:32:26 -0700

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,26 @@
+package assignments;
+
+public class Automation extends MultipleLanguage implements TestTool{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Selenium() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ruby() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
